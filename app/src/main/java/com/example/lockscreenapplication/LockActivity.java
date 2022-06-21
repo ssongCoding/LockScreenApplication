8f114131dedcbbde4b2122aa05b0ba4ab304d108
ssongCoding <songa.kim@kakao.com>
2022-06-21 18:45:58 +0900

    Day16

--- app/src/main/java/com/example/lockscreenapplication/LockActivity.java
+++ app/src/main/java/com/example/lockscreenapplication/LockActivity.java
@@ -2,6 +2,7 @@ package com.example.lockscreenapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -17,6 +18,7 @@ import java.util.concurrent.locks.Lock;
 public class LockActivity extends AppCompatActivity{
 
     EditText et_pwd1, et_pwd2, et_pwd3, et_pwd4;
+    String pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +31,27 @@ public class LockActivity extends AppCompatActivity{
         et_pwd3 = findViewById(R.id.et_pwd3);
         et_pwd4 = findViewById(R.id.et_pwd4);
 
-//        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-//        inputMethodManager.showSoftInput(et_pwd1, 0);
+        et_pwd1.addTextChangedListener(new MyTextWatcher(LockActivity.this, et_pwd2));
+        et_pwd2.addTextChangedListener(new MyTextWatcher(LockActivity.this, et_pwd3));
+        et_pwd3.addTextChangedListener(new MyTextWatcher(LockActivity.this, et_pwd4));
+        et_pwd4.addTextChangedListener(new MyTextWatcher(LockActivity.this, null));
 
-        et_pwd1.addTextChangedListener(new MyTextWatcher(et_pwd2));
-        et_pwd2.addTextChangedListener(new MyTextWatcher(et_pwd3));
-        et_pwd3.addTextChangedListener(new MyTextWatcher(et_pwd4));
+        pwd = et_pwd1.getText().toString() + et_pwd2.getText().toString()
+                + et_pwd3.getText().toString() + et_pwd4.getText().toString();
+
+        if (pwd.length() == 4) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(et_pwd4.getWindowToken(), 0);
+        }
     }
 }
 
 class MyTextWatcher implements TextWatcher {
     private EditText et_to;
+    private LockActivity lockActivity;
 
-    public MyTextWatcher(EditText et_to){
+    public MyTextWatcher(LockActivity activity, EditText et_to){
+        this.lockActivity = activity;
         this.et_to = et_to;
     }
 
@@ -52,7 +62,10 @@ class MyTextWatcher implements TextWatcher {
 
     @Override
     public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        if (charSequence.length() == 1) {
+        if (et_to == null){
+            lockActivity.setResult(Activity.RESULT_OK);
+            lockActivity.finish();
+        } else {
             et_to.requestFocus();
         }
     }
--- app/src/main/java/com/example/lockscreenapplication/MainActivity.java
+++ app/src/main/java/com/example/lockscreenapplication/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.lockscreenapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+//https://seongjaemoon.github.io/android/2017/12/04/androidActivity.html
+//https://jhshjs.tistory.com/49
+//https://androiddvlpr.com/android-calendar-library/
 public class MainActivity extends AppCompatActivity{
 
     Button btn_lock;
 
+    boolean isLocked = false;
+
+    /**
+     * 생명주기
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,9 +32,71 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, LockActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
     }
 
+    // Activity 뜨고, 가장 먼저 실행되는 메소드
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(isLocked){ // 잠금 설정이 되어 있으면,
+            Intent intent = new Intent(MainActivity.this, LockActivity.class);
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    // 사용자와 상호작용이 가능 할 때 호출 되는 메소드
+    // 예를 들면, 버튼을 클릭하고, 값을 넣고 등등 할 수 있을 때 호출 된다는 것!
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(isLocked){ // 잠금 설정이 되어 있으면,
+            Intent intent = new Intent(MainActivity.this, LockActivity.class);
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    // startActivityForResult 결과값을 받는 메소드
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Toast.makeText(getApplicationContext(), "WHHHHHHY", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getApplicationContext(), requestCode,
+//                Toast.LENGTH_SHORT).show();
+//
+//        Toast.makeText(getApplicationContext(), resultCode,
+//                Toast.LENGTH_SHORT).show();
+//
+        switch (requestCode) { // 내가 정해서 보낸 requestCode
+            case 1:
+                Toast.makeText(getApplicationContext(), "Hi",
+                        Toast.LENGTH_SHORT).show();
+                if (resultCode == Activity.RESULT_OK) {
+                    isLocked = true;
+                    break;
+                }
+        }
+    }
 }
+
+
